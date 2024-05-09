17ae43b8a05b15af68d5bc7bfb73782def754a25
IKala4I <pershkovladyslav@gmail.com>
2024-05-09 11:50:53 +0300

    * apartment model is updated

--- backend/models/apartment.model.ts
+++ backend/models/apartment.model.ts
@@ -1,10 +1,34 @@
 import {Schema, model} from 'mongoose';
 
 const ApartmentSchema = new Schema({
-    rooms: {type: Number, required: true},
-    name: {type: String, required: true},
-    price: {type: Number, required: true},
-    description: {type: String, required: true}
-});
+        rooms: {
+            type: Number,
+            min: 1,
+            required: true
+        },
+        name: {
+            type: String,
+            trim: true,
+            min: 1,
+            max: 98,
+            required: true
+        },
+        price: {
+            type: Number,
+            min: 1,
+            required: true
+        },
+        description: {
+            type: String,
+            trim: true,
+            min: 1,
+            max: 998,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
 
 export const Apartments = model('apartments', ApartmentSchema);
