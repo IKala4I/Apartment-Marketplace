d2dccbdcc13c6c4b931ab5dca370caed48dca89b
IKala4I <pershkovladyslav@gmail.com>
2024-05-10 23:07:10 +0300

    + filtration is added

--- frontend/src/app/dashboard/components/available-apartments/available-apartments-form/available-apartments-form.component.ts
+++ frontend/src/app/dashboard/components/available-apartments/available-apartments-form/available-apartments-form.component.ts
@@ -1,12 +1,14 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatFormField, MatLabel, MatSuffix} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {MatOption, MatSelect, MatSelectChange} from '@angular/material/select';
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ApartmentService} from 'src/app/common/services/apartment.service';
 import {LocalStorageService} from 'src/app/common/services/local-storage.service';
-import {Subject, takeUntil} from 'rxjs';
+import {debounceTime, distinctUntilChanged, Subject, takeUntil} from 'rxjs';
 import {getParamsFromLocalStorage} from 'src/app/utils/getParamsFromLocalStorage';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
 
 @Component({
   selector: 'ts-available-apartments-form',
@@ -18,7 +20,10 @@ import {getParamsFromLocalStorage} from 'src/app/utils/getParamsFromLocalStorage
     MatSelect,
     MatOption,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatIcon,
+    MatIconButton,
+    MatSuffix
   ],
   templateUrl: './available-apartments-form.component.html',
   styleUrl: './available-apartments-form.component.scss'
@@ -28,6 +33,12 @@ export class AvailableApartmentsFormComponent implements OnInit, OnDestroy {
   manageForm: FormGroup;
 
   private untilSubject$ = new Subject<void>();
+
+  get roomsValue() {
+    const roomsControl = this.manageForm.get('rooms');
+    console.log(roomsControl?.value);
+    return roomsControl?.value ? roomsControl?.value : null;
+  }
 
   constructor(private fb: FormBuilder, private apartmentService: ApartmentService, private localStorageService: LocalStorageService) {
   }
@@ -43,6 +54,18 @@ export class AvailableApartmentsFormComponent implements OnInit, OnDestroy {
       price: [params['price'] ? params['price'] : null],
       rooms: [params['rooms'] ? params['rooms'] : null]
     });
+
+    this.manageForm.get('rooms')?.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        takeUntil(this.untilSubject$),
+      )
+      .subscribe(value => {
+        if (value)
+          this.localStorageService.saveData('rooms', value);
+        this.apartmentService.getApartments();
+      });
   }
 
   public sortApartments(event: MatSelectChange) {
@@ -51,6 +74,17 @@ export class AvailableApartmentsFormComponent implements OnInit, OnDestroy {
     else
       this.localStorageService.deleteData('price');
 
+    this.apartmentService.getApartments()
+      .pipe(
+        takeUntil(this.untilSubject$)
+      )
+      .subscribe(() => {
+      });
+  }
+
+  public clearRooms() {
+    this.localStorageService.deleteData('rooms');
+    this.manageForm.get('rooms')?.setValue(null);
     this.apartmentService.getApartments()
       .pipe(
         takeUntil(this.untilSubject$)
