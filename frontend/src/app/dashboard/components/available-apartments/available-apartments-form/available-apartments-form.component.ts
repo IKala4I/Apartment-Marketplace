a0a61f4dfe7778109b9c5e627e937764b2240947
IKala4I <pershkovladyslav@gmail.com>
2024-05-10 20:29:01 +0300

    + filtration and sorting form is added

--- frontend/src/app/dashboard/components/available-apartments/available-apartments-form/available-apartments-form.component.ts
+++ frontend/src/app/dashboard/components/available-apartments/available-apartments-form/available-apartments-form.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MatOption, MatSelect} from '@angular/material/select';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'ts-available-apartments-form',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatSelect,
+    MatOption,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './available-apartments-form.component.html',
   styleUrl: './available-apartments-form.component.scss'
 })
